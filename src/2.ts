a4f4c3553fcf6427c0bbdf5c8c536f503012654b
willsdewhirst738 <willsdewhirst738@gmail.com>
2025-03-07 10:10:03 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,29 @@
+
+const schoolEvents = {
+  "events": [
+    {
+      "id": 1,
+      "name": "School Event",
+      "startDate": new Date("2023-03-25"),
+      "endDate": new Date("2023-04-29"),
+      "location": "The School Hall",
+      "description": "Annual school event to celebrate the school's achievements and goals."
+    },
+    {
+      "id": 2,
+      "name": "Sports Day",
+      "startDate": new Date("2023-03-26"),
+      "endDate": new Date("2023-04-28"),
+      "location": "The School Stadium",
+      "description": "A sports competition to showcase the school's athletic talents."
+    },
+    {
+      "id": 3,
+      "name": "Music Festival",
+      "startDate": new Date("2023-04-15"),
+      "endDate": new Date("2023-04-29"),
+      "location": "The School Auditorium",
+      "description": "An annual music festival to showcase the school's musical talents."
+    }
+  ]
+};
