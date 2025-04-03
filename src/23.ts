99abae0a0ba49f6cd95b60222b5acd9385601151
willsdewhirst738 <willsdewhirst738@gmail.com>
2025-04-03 13:07:24 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,6 @@
+function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+console.log(getRandomInt(10) + getRandomInt(15)); // Output: 20 or 35
