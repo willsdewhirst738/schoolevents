2821ffc18474bfab054ff2bf18a74822b1a9b4fc
willsdewhirst738 <willsdewhirst738@gmail.com>
2025-04-09 02:32:42 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,6 @@
+function getRandomNumber(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
